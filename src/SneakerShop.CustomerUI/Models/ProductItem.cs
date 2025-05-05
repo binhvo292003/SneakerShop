3afde8942596b6554910af8b35ea988b4ada3775
binhvo292003 <95414345+binhvo292003@users.noreply.github.com>
2025-05-06 03:24:23 +0700

    fix: slug error

--- src/SneakerShop.CustomerUI/Models/ProductItem.cs
+++ src/SneakerShop.CustomerUI/Models/ProductItem.cs
@@ -7,6 +7,6 @@ namespace SneakerShop.CustomerUI.Models
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public string Slug { get; set; } = "1";
+        public string Slug { get; set; }
     }
 }
